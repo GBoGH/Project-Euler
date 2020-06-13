1277f38eff31680b7ef816e057b5fa4e5df98748
Jakub Feik <feikjakub@gmail.com>
2020-06-13 18:03:18 +0200

    merging changes

--- Project Euler.py
+++ Project Euler.py
@@ -41,7 +41,6 @@ def ch3(x):
 
 
 # Challenge number 4.
-# UNFINISHED
 palindromes = []
 def ch4():
     for i in range(100,1000):
@@ -52,3 +51,30 @@ def ch4():
 
                 
 
+# Challenge number 5.
+#NOT COMPLETE
+def ch5():
+    num = 20
+    i = 2
+    while i <= 20:
+        while num % i == 0:
+            i += 1
+        num += 1
+    print(num)
+
+ch5()
+
+"""def ifDividesAll(num):
+  for i in range(2,21):
+    if num % i != 0:
+      return False
+  return True
+
+num = 20
+while True:
+  if ifDividesAll(num):
+    break
+  else:
+    num = num + 1
+print num
+"""
