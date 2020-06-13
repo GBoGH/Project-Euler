59660ebecc44b9dc270297cdd5c9f21283e75637
Jakub Feik <feikjakub@gmail.com>
2020-06-13 20:04:47 +0200

    testing and bugs removal

--- Project Euler.py
+++ Project Euler.py
@@ -1,6 +1,5 @@
 # Challenge number 1.
 multiples = []
-
 def ch1(x):
     for i in range(x):
         if i % 3 == 0:
@@ -11,13 +10,12 @@ def ch1(x):
 
     print(sum(multiples))
 
-#ch1(1000)
+ch1(1000)
 
 
 # Challenge number 2.
 numbers = [0, 1]
 evens = []
-
 def ch2(x):
     while (numbers[-1] + numbers[-2]) < x:
         numbers.append((numbers[-1] + numbers[-2]))
@@ -25,7 +23,7 @@ def ch2(x):
             evens.append((numbers[-1] + numbers[-2]))
     print(sum(evens))
 
-#ch2(4000000)
+ch2(4000000)
 
 
 # Challenge number 3.
@@ -36,23 +34,23 @@ def ch3(x):
             x /= i
         i += 1
     print(int(x))
-        
-#ch3(600851475143)
 
+ch3(600851475143)
 
-# Challenge number 4.
+#Challenge number 4.
 palindromes = []
 def ch4():
-    for i in range(100,1000):
-        for j in reversed(range(100,1000)):
-            while i != j:
-                x = i * j
-                print(x)
+    for j in range(100,1000):
+        x = str(i * j)
+        if len(x) == 6 and x == x[::-1]:
+            palindromes.append(x)
 
-                
+    print(max(palindromes))
+
+ch4()
 
 # Challenge number 5.
-def ch5():  
+def ch5():
     num = 2520
     i = 2
     while i <= 20:
@@ -63,17 +61,19 @@ def ch5():
             i = 2
     print(num)
 
-#ch5()
+ch5()
 
 
 # Challenge number 6.
 numbers = []
 squares = []
 def ch6():
-    for i in range(1,101):
+    for i in range(1, 101):
         squares.append(i**2)
         numbers.append(i)
     result = sum(numbers)**2 - sum(squares)
     print(result)
 
 ch6()
+
+
