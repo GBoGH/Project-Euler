34a4682f1737e0bd14044b3305512c361e6349f1
Jakub Feik <feikjakub@gmail.com>
2020-06-13 18:00:38 +0200

    progress with #5

--- Project Euler.py
+++ Project Euler.py
@@ -53,15 +53,29 @@ def ch4():
 
 
 # Challenge number 5.
-#UNFINISHED
+#NOT COMPLETE
 def ch5():
-    for i in range(1, 1000000):
-        j = 1
-        while j <= 20:
-            if i % j == 0:
-                j += 1
-            else:
-                continue
-            print(i)
+    num = 20
+    i = 2
+    while i <= 20:
+        while num % i == 0:
+            i += 1
+        num += 1
+    print(num)
 
 ch5()
+
+"""def ifDividesAll(num):
+  for i in range(2,21):
+    if num % i != 0:
+      return False
+  return True
+
+num = 20
+while True:
+  if ifDividesAll(num):
+    break
+  else:
+    num = num + 1
+print num
+"""
